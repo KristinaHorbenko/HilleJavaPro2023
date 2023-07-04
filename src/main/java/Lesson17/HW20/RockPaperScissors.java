e1036c638f27eee3e264dcbce922f3cab093541c
Kristina <tina.kotlyrova@gmail.com>
2023-07-05 00:01:03 +0300

    second commit

---
+++ src/main/java/Lesson17/HW20/RockPaperScissors.java
@@ -0,0 +1,8 @@
+package Lesson17.HW20;
+
+public class RockPaperScissors {
+    private static final int Rock = 1;
+    private static final int Paper = 2;
+    private static final int Scissors = 3;
+
+}
